cd31b7a32aa8e5b475d6cf7ba370b0a8bb8d92b8
Yusuf Yıldız <ysfyldz0077@gmail.com>
2022-09-23 16:09:46 +0300

    add ipinfo api

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const url = "https://api.openweathermap.org/data/2.5/"
 const apikey = "84c05e93b5c3a4134c3f321d118a5357"
 
+async function fetchText() {
+    let ipurl = "https://ipinfo.io/json?token=68dd71c1eba45f";
+    let response = await fetch(ipurl);
+    let data = await response.json();
+    getResult(data.city)
+} fetchText()
+
 const setQuery = (e) => {
     if(e.keyCode == '13'){
         getResult(searchbar.value)
     }
 }
+
 
 const getResult = (cityName) => {
     let query = `${url}weather?q=${cityName}&appid=${apikey}&units=metric&lang=tr`
@@ -19,17 +27,20 @@ const getResult = (cityName) => {
 const displayresult = (result) => {
     let city = document.querySelector('.city')
     city.innerText = `${result.name}, ${result.sys.country}`
-
+    
     let temp = document.querySelector('.temp')
     temp.innerText = `${Math.round(result.main.temp)}°C`
-
+    
     let desc = document.querySelector('.desc')
     desc.innerText = result.weather[0].description
-
-
+    
+    
     let minmax = document.querySelector(".minmax")
     minmax.innerText = `${Math.round(result.main.temp_min)}°C / ${Math.round(result.main.temp_max)}°C`
 }
+
+window.addEventListener("load", () => {
+})
 
 const searchbar = document.getElementById("searchBar")
 searchbar.addEventListener("keypress", setQuery)
