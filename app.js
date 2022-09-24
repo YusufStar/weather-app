5a38d8dfaf09c820171cbeebaa60e421d95623b7
Yusuf Yıldız <ysfyldz0077@gmail.com>
2022-09-24 10:29:01 +0300

    done

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const url = "https://api.openweathermap.org/data/2.5/"
 const apikey = "84c05e93b5c3a4134c3f321d118a5357"
+var icon = document.getElementsByClassName("img")
+
 
 async function fetchText() {
     let ipurl = "https://ipinfo.io/json?token=68dd71c1eba45f";
@@ -22,25 +24,24 @@ const getResult = (cityName) => {
         return weather.json()
     })
     .then(displayresult)
-} 
-
-const displayresult = (result) => {
-    let city = document.querySelector('.city')
-    city.innerText = `${result.name}, ${result.sys.country}`
-    
-    let temp = document.querySelector('.temp')
-    temp.innerText = `${Math.round(result.main.temp)}°C`
-    
-    let desc = document.querySelector('.desc')
-    desc.innerText = result.weather[0].description
-    
-    
-    let minmax = document.querySelector(".minmax")
-    minmax.innerText = `${Math.round(result.main.temp_min)}°C / ${Math.round(result.main.temp_max)}°C`
 }
 
-window.addEventListener("load", () => {
+const button = document.getElementById("button")
+button.addEventListener("click", () => {
+    getResult(searchbar.value)
 })
+
+const displayresult = (result) => {
+    console.log(result)
+    document.getElementById("time").innerHTML = new Date().toDateString()
+    document.getElementById("city").innerHTML = `${result.name}, ${result.sys.country}`
+    document.getElementById("temp").innerHTML = `${Math.round(result.main.temp)}°`
+    document.getElementById("weather").innerHTML = result.weather[0].description
+    document.getElementById("cloud").innerHTML = `${result.clouds.all}%`
+    document.getElementById("humidity").innerHTML = `${result.main.humidity}%`
+    document.getElementById("wind").innerHTML = `${Math.round(result.wind.speed)}km/h`
+    icon[0].src = `http://openweathermap.org/img/wn/${result.weather[0].icon}.png`
+}
 
 const searchbar = document.getElementById("searchBar")
 searchbar.addEventListener("keypress", setQuery)
